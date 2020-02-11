1e21a63a5a027efa30d24b2e1183aae69434b27d
BlueC0C0 <BlueC0C0@users.github.com>
2020-02-11 21:34:37 +0100

    ajout de code en rapport avec le thread

--- GobbletGobblers/src/Main1.java
+++ GobbletGobblers/src/Main1.java
@@ -1,7 +1,11 @@
 
 public class Main1 {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World");
-	}
+  public static void main(String[] args) {
+    TestThread t = new TestThread("A");
+    TestThread t2 = new TestThread("  B");
+    TestThread t3 = new TestThread("    C");
+    t.start();
+    t2.start();
+    t3.start();
+  }
 }
