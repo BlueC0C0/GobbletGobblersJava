8f45763aa037289974180fee93bb8e3dc372d529
BlueC0C0 <BlueC0C0@users.github.com>
2020-02-13 19:12:59 +0100

    ajout du projet avec forme

--- GobbletGobblers/src/Main1.java
+++ GobbletGobblers/src/Main1.java
@@ -1,11 +1,6 @@
 
 public class Main1 {
   public static void main(String[] args) {
-    TestThread t = new TestThread("A");
-    TestThread t2 = new TestThread("  B");
-    TestThread t3 = new TestThread("    C");
-    t.start();
-    t2.start();
-    t3.start();
+    Fenetre f1 = new Fenetre();
   }
 }
